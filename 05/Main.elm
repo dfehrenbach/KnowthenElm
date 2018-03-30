d74d5fee59bb4719fb9788bd81c960f8625a8d5f
Daniel Fehrenbach <dmf8646@rit.edu>
2018-03-29 21:17:26 -0500

    Iteration 2
    - Player Section added to the View

--- 05/Main.elm
+++ 05/Main.elm
@@ -142,6 +142,85 @@ view model =
         ]
 
 
+playerListHeader : Html Msg
+playerListHeader =
+    header []
+        [ div [] [ text "Name" ]
+        , div [] [ text "Points" ]
+        ]
+
+
+playerList : Model -> Html Msg
+playerList model =
+    model.players
+        |> List.sortBy .name
+        |> List.map player
+        |> ul []
+
+
+player : Player -> Html Msg
+player player =
+    li []
+        [ i
+            [ class "edit"
+            , onClick (Edit player)
+            ]
+            []
+        , div []
+            [ text player.name ]
+        , button
+            [ type_ "button"
+            , onClick (Score player 2)
+            ]
+            [ text "2pt" ]
+        , button
+            [ type_ "button"
+            , onClick (Score player 3)
+            ]
+            [ text "3pt" ]
+        , div []
+            [ text (toString player.points) ]
+        ]
+
+
+pointTotal : Model -> Html Msg
+pointTotal model =
+    let
+        total =
+            model.plays
+                |> List.map .points
+                |> List.sum
+    in
+        footer []
+            [ div [] [ text "Total:" ]
+            , div [] [ text (toString total) ]
+            ]
+
+
+playerSection : Model -> Html Msg
+playerSection model =
+    div []
+        [ playerListHeader
+        , playerList model
+        , pointTotal model
+        ]
+
+
+playerForm : Model -> Html Msg
+playerForm model =
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type_ "text"
+            , placeholder "Add/Edit Player..."
+            , onInput Input
+            , value model.name
+            ]
+            []
+        , button [ type_ "submit" ] [ text "Save" ]
+        , button [ type_ "button", onClick Cancel ] [ text "Cancel" ]
+        ]
+
+
 main : Program Never Model Msg
 main =
     Html.beginnerProgram
